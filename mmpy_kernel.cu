a8b4ef14fe839f7da293afc9045f8f69ba0465d3
shaoweiliu <shaoweiliu@wireless-169-228-109-250.ucsd.edu>
2020-02-13 11:53:31 -0800

    first commit

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -3,19 +3,32 @@
 #include <math.h>
 #include "utils.h"
 #include "types.h"
+
+#define BLOCK_SIZE 32 # BLOCK_SIZE=BLOCK_DIM_X=BLOCK_DIM_Y
+
 using namespace std;
 
 __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 
-    int I =  blockIdx.y*blockDim.y + threadIdx.y;
-    int J =  blockIdx.x*blockDim.x + threadIdx.x;
+    __shared__ double As[BLOCK_SIZE][BLOCK_SIZE], Bs[BLOCK_SIZE][BLOCK_SIZE];
 
-    if((I < N) && (J < N)){
-        _DOUBLE_ _c = 0;
-        for (unsigned int k = 0; k < N; k++) {
-            _DOUBLE_ a = A[I * N + k];
-            _DOUBLE_ b = B[k * N + J];
-            _c += a * b;
+    int I =  blockIdx.y*BLOCK_SIZE + threadIdx.y;
+    int J =  blockIdx.x*BLOCK_SIZE + threadIdx.x;
+
+    _DOUBLE_ _c = 0;
+
+    if((I < N) && (J < N))
+    {
+        for (int kk=0; kk<N/BLOCK_SIZE; kk++)
+        {
+            As[threadIdx.y][threadIdx.x] = A[I*N + kk*BLOCK_SIZE + threadIdx.x];
+            Bs[threadIdx.y][threadIdx.x] = B[(kk*BLOCK_SIZE+threadIdx.y) * N + J];
+            __syncthreads();
+            for (int k=0; k<BLOCK_SIZE; k++)
+            {
+                _c += As[threadIdx.y][k] * Bs[k][threadIdx.x]
+            }
+            __syncthreads();
         }
         C[I * N + J] = _c;
     }
