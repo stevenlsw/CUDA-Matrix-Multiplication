920aae493945881da33857531d957d6db73fb8c8
shaoweiliu <shaoweiliu@shaoweilius-MacBook-Pro.local>
2020-02-15 14:45:25 -0800

    shared-memory

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -4,7 +4,11 @@
 #include "utils.h"
 #include "types.h"
 
-#define BLOCK_SIZE 32 # BLOCK_SIZE=BLOCK_DIM_X=BLOCK_DIM_Y
+#define BLOCK_SIZE 32 // BLOCK_SIZE=BLOCK_DIM_X=BLOCK_DIM_Y
+
+#define MAT_ELEMENT(mat, N, i, j) ((i)<N && (j)<N ? mat[(i)*N+(j)] : 0)
+#define A_ELEMENT(i, j) MAT_ELEMENT(A, N, i, j)
+#define B_ELEMENT(i, j) MAT_ELEMENT(B, N, i, j)
 
 using namespace std;
 
@@ -12,24 +16,31 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 
     __shared__ double As[BLOCK_SIZE][BLOCK_SIZE], Bs[BLOCK_SIZE][BLOCK_SIZE];
 
-    int I =  blockIdx.y*BLOCK_SIZE + threadIdx.y;
-    int J =  blockIdx.x*BLOCK_SIZE + threadIdx.x;
+    int tx = threadIdx.x, ty = threadIdx.y;
+    itn bx = blockIdx.x, by = blockIdx.y;
 
-    _DOUBLE_ _c = 0;
+    int I =  by * BLOCK_SIZE+ ty, J =  bx * BLOCK_SIZE + tx;
+
+    _DOUBLE_ c = 0;
 
     if((I < N) && (J < N))
     {
-        for (int kk=0; kk<N/BLOCK_SIZE; kk++)
+        for (int kk=0; kk<N/BLOCK_SIZE+1; kk++)
         {
-            As[threadIdx.y][threadIdx.x] = A[I*N + kk*BLOCK_SIZE + threadIdx.x];
-            Bs[threadIdx.y][threadIdx.x] = B[(kk*BLOCK_SIZE+threadIdx.y) * N + J];
+            // load I,K of A
+            As[ty][tx] = A_ELEMENT(I, kk*BLOCK_SIZE + tx);
+            // load K,J of B
+            Bs[ty][tx] = B_ELEMENT(kk*BLOCK_SIZE + ty, J);
+
             __syncthreads();
+
+            // Compute I,J of C
             for (int k=0; k<BLOCK_SIZE; k++)
             {
-                _c += As[threadIdx.y][k] * Bs[k][threadIdx.x]
+                c += As[ty][k] * Bs[k][tx];
             }
             __syncthreads();
+            C[I * N + J] = c;
         }
-        C[I * N + J] = _c;
     }
 }
