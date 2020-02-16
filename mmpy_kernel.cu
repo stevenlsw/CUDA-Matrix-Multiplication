49f568061ad5a3874163c9f5d8fe59a09a55d220
shaoweiliu <shaoweiliu@shaoweilius-MacBook-Pro.local>
2020-02-15 22:18:56 -0800

    unroll

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -36,6 +36,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
 
     _DOUBLE_ c[Y_SUB][X_SUB] = {0}; // Zero initialize the whole array
 
+    #pragma unroll
     for (int kk=0; kk<N/BLOCK_SIZE_K+1; kk++)
     {
             // load corresponding values of A in the matrix block
@@ -60,6 +61,7 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B)
 
         __syncthreads();
 
+        #pragma unroll
         for (int k=0; k<BLOCK_SIZE_K; k++)
         {
             #pragma unroll
