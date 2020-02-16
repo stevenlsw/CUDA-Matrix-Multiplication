76c235951bf053ef847cef847ad9afe8ef3a6a5a
shaoweiliu <shaoweiliu@shaoweilius-MacBook-Pro.local>
2020-02-15 18:51:59 -0800

    ILP

--- mmpy_kernel.cu
+++ mmpy_kernel.cu
@@ -4,15 +4,25 @@
 #include "utils.h"
 #include "types.h"
 
-#define BLOCK_SIZE 32 // BLOCK_SIZE=BLOCK_DIM_X=BLOCK_DIM_Y
+#define BLOCK_SIZE 32 // matrix block
+#if BLOCK_SIZE % BLOCKDIM_X || BLOCK_SIZE % BLOCKDIM_Y
+#error BLOCK_SIZE must be multiple of blockDim
+#endif
 
-#define MAT_ELEMENT(mat, N, i, j) ((i)<N && (j)<N ? mat[(i)*N+(j)] : 0)
-#define A_ELEMENT(i, j) MAT_ELEMENT(A, N, i, j)
-#define B_ELEMENT(i, j) MAT_ELEMENT(B, N, i, j)
+// Number of instructions Computing in each thread
+#define X_SUB (BLOCK_SIZE / BLOCKDIM_X)
+#define Y_SUB (BLOCK_SIZE / BLOCKDIM_Y)
+
+#define MAT(mat, N, i, j) (mat[(i)*N + (j)])
+#define MAT_PADDED(mat, N, i, j) ((i) < N && (j) < N ? MAT(mat, N, i, j) : 0)
+#define A_ELEMENT(i, j) MAT_PADDED(A, N, i, j)
+#define B_ELEMENT(i, j) MAT_PADDED(B, N, i, j)
+#define C_ELEMENT(i, j) MAT(C, N, i, j)
 
 using namespace std;
 
-__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
+__global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) 
+{
 
     __shared__ double As[BLOCK_SIZE][BLOCK_SIZE], Bs[BLOCK_SIZE][BLOCK_SIZE];
 
@@ -21,26 +31,52 @@ __global__ void matMul(int N, _DOUBLE_ *C, _DOUBLE_ *A, _DOUBLE_ *B) {
 
     int I =  by * BLOCK_SIZE+ ty, J =  bx * BLOCK_SIZE + tx;
 
-    _DOUBLE_ c = 0;
+    _DOUBLE_ c[Y_SUB][X_SUB] = {0}; // Zero initialize the whole array
 
-    if((I < N) && (J < N))
+
+    for (int kk=0; kk<N/BLOCK_SIZE+1; kk++)
     {
-        for (int kk=0; kk<N/BLOCK_SIZE+1; kk++)
-        {
-            // load I,K of A
-            As[ty][tx] = A_ELEMENT(I, kk*BLOCK_SIZE + tx);
-            // load K,J of B
-            Bs[ty][tx] = B_ELEMENT(kk*BLOCK_SIZE + ty, J);
-
-            __syncthreads();
-
-            // Compute I,J of C
-            for (int k=0; k<BLOCK_SIZE; k++)
-            {
-                c += As[ty][k] * Bs[k][tx];
+            // load corresponding values of A in the matrix block
+        for (int i = 0; i < BLOCK_SIZE; i += BLOCKDIM_Y) {
+            for (int j = 0; j < BLOCK_SIZE; j += BLOCKDIM_X) {
+                // load I,K of A, As[ty][tx] = A_ELEMENT(I, kk*BLOCK_SIZE + tx);
+                As[ty + i][tx + j] = A_ELEMENT(I + ty + i, kk*BLOCK_SIZE + tx + j);
             }
-            __syncthreads();
-            C[I * N + J] = c;
+        }
+        
+        // load corresponding values of B in the matrix block
+        for (int i = 0; i < BLOCK_SIZE; i += BLOCKDIM_Y) {
+            for (int j = 0; j < BLOCK_SIZE; j += BLOCKDIM_X) {
+                // load K,J of B, Bs[ty][tx] = B_ELEMENT(kk*BLOCK_SIZE + ty, J);
+                Bs[ty + i][tx + j] = B_ELEMENT(kk*BLOCK_SIZE + ty + i, J + tx + j);
+            }
+        }
+
+        __syncthreads();
+
+        for (int k=0; k<BLOCK_SIZE; k++)
+        {
+            for (int i = 0; i < Y_SUB; i++) 
+            {
+                for (int j = 0; j < X_SUB; j++) 
+                {
+                    // Compute I,J of C, c += As[ty][k] * Bs[k][tx];
+                    c[i][j] += As[ty + i * BLOCKDIM_Y][k] * Bs[k][tx + j * BLOCKDIM_X];
+                }
+            }
+        }
+
+        __syncthreads();
+
+        for (int i = 0; i < Y_SUB; ++i) 
+        {
+            for (int j = 0; j < X_SUB; ++j) 
+            {
+                if (I + ty + i * BLOCKDIM_Y < N && J + tx + j * BLOCKDIM_X < N)
+                {
+                    C_ELEMENT(I + ty + i * BLOCKDIM_Y, J + tx + j * BLOCKDIM_X) = c[i][j];
+                }                
+            }
         }
     }
 }
